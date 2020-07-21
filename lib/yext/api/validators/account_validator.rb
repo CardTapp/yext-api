a31fdb2c07a3d636e96920a29da7160f87f77010
RealNobody <RealNobody1@cox.net>
2020-07-20 22:27:55 -0700

    Gem updates found in recent spec run issues

--- lib/yext/api/validators/account_validator.rb
+++ lib/yext/api/validators/account_validator.rb
@@ -4,8 +4,9 @@ module Yext
   module Api
     module Validators
       # A Validator for account updates to verify that the update is valid.
-      class AccountValidator < Validation::Validator
-        include Validation
+      class AccountValidator < ::Validation::Validator
+        include ::Validation
+        extend ::Validation::Rules
 
         def initialize(obj)
           # Ensure that the accountId field exists by cloning the Account and setting it.
